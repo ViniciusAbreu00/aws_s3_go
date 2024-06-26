eea8a5d3cf9359b136e0c40feed6e77d6c5a3681
ViniciusAbreu00 <viniciusabreu00@outlook.com>
2024-06-26 19:50:12 -0300

    feat; add cli interface to manipulate functions

--- bucket_service/bucket_service.go
+++ bucket_service/bucket_service.go
@@ -23,29 +23,30 @@ func inits3Client() {
 
 }
 
-func GetBucketContent() error {
+func GetBucketContent() ([]string, error) {
 	if s3client == nil {
 		inits3Client()
 	}
 
 	output, err := s3client.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input{Bucket: aws.String("seiglu-solutions")})
 	if err != nil {
-		return err
+		return nil, err
 	}
 
+	var data []string
 	for _, object := range output.Contents {
-		log.Printf("key=%s size=%d", aws.ToString(object.Key), object.Size)
+		data = append(data, fmt.Sprintf("key=%s size=%d", aws.ToString(object.Key), object.Size))
 	}
 
-	return nil
+	return data, nil
 }
 
-func UploadFile(filename string) (string, error) {
+func UploadFile(pathname string) (string, error) {
 	if s3client == nil {
 		inits3Client()
 	}
 
-	file, err := os.Open(filename)
+	file, err := os.Open(pathname)
 	if err != nil {
 		return "", err
 	}
@@ -54,14 +55,14 @@ func UploadFile(filename string) (string, error) {
 
 	_, err = s3client.PutObject(context.TODO(), &s3.PutObjectInput{
 		Bucket: aws.String("seiglu-solutions"),
-		Key:    aws.String(filename),
+		Key:    aws.String(pathname),
 		Body:   file,
 	})
 	if err != nil {
 		return "", err
 	}
 
-	message := fmt.Sprintf("File uploaded successfully: %s", filename)
+	message := fmt.Sprintf("File uploaded successfully: %s", pathname)
 
 	return message, nil
 }
--- main.go
+++ main.go
@@ -2,21 +2,54 @@ package main
 
 import (
 	"aws_s3_go/bucket_service"
-	"fmt"
 	"log"
+
+	"github.com/spf13/cobra"
 )
 
-func searchAndUploadFile(bucket, key, filename string, mode string) string {
-	switch mode {
-	case "upload":
-		mess, err := bucket_service.UploadFile(filename)
-		if err != nil {
-			log.Fatalf("error uploading file: %f", err)
-		}
-		return mess
-	case "search":
-		return searchFile(bucket, key, filename)
-	default:
-		return fmt.Errorf("invalid mode, must be 'upload' or 'search'")
+func main() {
+	var pathname string
+
+	rootCmd := &cobra.Command{
+		Use:   "seiglu",
+		Short: "Seiglu CLI is a tool for uploading and searching files in a aws bucket",
 	}
+
+	uploadCmd := &cobra.Command{
+		Use:   "upload",
+		Short: "Uploads a file to a aws bucket",
+		Run: func(cmd *cobra.Command, args []string) {
+			if pathname == "" {
+				log.Fatalf("pathname must be provided")
+			} else {
+				bucket_service.UploadFile(pathname)
+			}
+
+		},
+	}
+
+	uploadCmd.Flags().StringVarP(&pathname, "path", "p", "", "path to upload")
+	uploadCmd.MarkFlagRequired("path")
+
+	searchCmd := &cobra.Command{
+		Use:   "search",
+		Short: "Search for a file in the bucket",
+		Run: func(cmd *cobra.Command, args []string) {
+			data, error := bucket_service.GetBucketContent()
+			if error != nil {
+				log.Fatal(error)
+			}
+			for _, item := range data {
+				log.Println(item)
+			}
+		},
+	}
+
+	rootCmd.AddCommand(uploadCmd)
+	rootCmd.AddCommand(searchCmd)
+
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatal(err)
+	}
+
 }
